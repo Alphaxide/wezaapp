820086a9886c41899c0b57a3e6bb1980c245fc95
microstrategythis <wellbeingtoday@proton.me>
2025-04-18 15:03:32 +0300

    build gradle file changes

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,6 +11,8 @@ android {
     ndkVersion = "27.0.12077973"
 
     compileOptions {
+        // Add this line for desugaring
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -41,4 +43,9 @@ android {
 
 flutter {
     source = "../.."
+}
+
+// Add this dependencies block at the end
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
 }
